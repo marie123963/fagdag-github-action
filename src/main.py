fa3552d4c0157bbe42e9d245ce0c0b9f214ceb91
henriette.rebnor <henriette.rebnor@bouvet.no>
2025-02-12 21:21:06 +0100

    fun!

--- src/main.py
+++ src/main.py
@@ -7,15 +7,17 @@ if __name__ == '__main__':
     load_dotenv()
     acces_token = os.environ.get('GITHUB_TOKEN')
     repo_uri = os.environ.get('GITHUB_REPOSITORY')
-    
+    pr_number = os.getenv('GITHUB_REF').split('/')[-1]
+
     if(repo_uri is None or acces_token is None):
         raise Exception('Could not find repository')
     token = Auth.Token(acces_token)
 
     github = Github(auth=token)
     repo = github.get_repo(repo_uri)
+
+    pull_request = repo.get_pull(pr_number)
+    commits = pull_request.get_commits()
     
-    latest = repo.get_commits()[0]
-    message = latest.commit.message
-    
-    
+    for c in commits:
+        print(c.commit.message)
