bd845f9b2726ea1f10fb14d703d5712233908650
henriette.rebnor <henriette.rebnor@bouvet.no>
2025-02-12 20:59:14 +0100

    testing

---
+++ README.md
@@ -0,0 +1,3 @@
+get git token windows: ```"protocol=https`nhost=github.com`n" | git credential fill``` 
+get git token linux: ``` echo -e "protocol=https\nhost=github.com\n" | git credential fill ```
+DO NOT share this, paste it in the env file. 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 from github import Github, Auth
-from openai import OpenAI
 from dotenv import load_dotenv
 
 if __name__ == '__main__':
@@ -8,7 +7,6 @@ if __name__ == '__main__':
     load_dotenv()
     acces_token = os.environ.get('GITHUB_TOKEN')
     repo_uri = os.environ.get('GITHUB_REPOSITORY')
-    client = OpenAI(); 
     
     if(repo_uri is None or acces_token is None):
         raise Exception('Could not find repository')
@@ -17,4 +15,5 @@ if __name__ == '__main__':
     github = Github(auth=token)
     repo = github.get_repo(repo_uri)
     
-    
+    latest = repo.get_commits()[0]
+    message = latest.commit.message
