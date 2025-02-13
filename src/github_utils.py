d707a0876248ab260461af62bcb17b7199bb72ba
henriette.rebnor <henriette.rebnor@bouvet.no>
2025-02-13 15:57:11 +0100

    index list

--- src/github_utils.py
+++ src/github_utils.py
@@ -1,7 +1,7 @@
 from github import PullRequest, InputGitAuthor, Repository
 
 def get_latest_commit_message(pull_request : PullRequest) -> list[str]:
-     return [c.commit.message for c in pull_request.get_commits()]
+    return [c.commit.message for c in pull_request.get_commits()][-1]        
 
 def commit_and_push(repo: Repository, target_branch:str, file_path: str) -> None:
     author = InputGitAuthor(
